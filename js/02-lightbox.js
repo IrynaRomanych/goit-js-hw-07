56c11f03dbdd4db9de9e3cfe20223fa6dfe3fe22
IrynaRomanych <106173463+IrynaRomanych@users.noreply.github.com>
2022-10-19 22:48:29 +0300

    finished hw-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,38 +4,29 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const gallerySet = document.querySelector('.gallery');
+
 const galleryMarkUp = createGalleryMarkUp(galleryItems);
+
 gallerySet.insertAdjacentHTML('afterbegin', galleryMarkUp);
-gallerySet.addEventListener('click', onImgClick);
+// gallerySet.addEventListener('click', onImgClick);
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
 function createGalleryMarkUp(galleryItems) {
     return galleryItems
         .map(
-            ({ description, preview, original }) => `<div class="gallery__item">
-    <a class="gallery__item" href=${original}>
+            ({ description, preview, original }) => `<li>
+    <a class="gallery__item" href="${original}">
         <img
             class="gallery__image"
             data-source="${original}"
             src="${preview}"
             alt="${description}"
         />
-    </a>
-</div>`
+    </a></li>
+`
         )
         .join('');
-}
-
-function onImgClick(event) {
-    event.preventDefault();
-    if (event.target.tagName !== 'IMG') {
-        return;
-    }
-    const lightbox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-    });
-
-    document.addEventListener('keydown', event => {
-        if (event.key === 'Escape') instance.close();
-    });
 }
